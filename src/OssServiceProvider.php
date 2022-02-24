7262e7469f6a47f01443d8e07d81dd3d42111957
kaysonwu <772663139@qq.com>
2022-02-24 14:29:22 +0800

    chores:update service provider

--- src/OssServiceProvider.php
+++ src/OssServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Illusionist\Flysystem\Aliyun;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 use OSS\OssClient;
@@ -15,27 +14,38 @@ class OssServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+    {  
+        $this->registerDriver();
+    }
+
+    /**
+     * Register the aliyun oss driver for the files system.
+     * 
+     * @return void
+     */
+    protected function registerDriver()
     {
-        Storage::extend('aliyun-oss', function ($app, $config) {
-
-            $client = @new OssClient(
-                $config['key'],
-                $config['secret'],
-                ($url = $config['endpoint']),
-                $config['cname'],
-                $config['token']
-            );
-
-            if (isset($config['url'])) {
-                $url = $config['url'];
-            } elseif ($config['cname'] === false) {
-                $url = '';
-            }
-
-            $prefix = isset($config['prefix']) ? $config['prefix'] : '';
-            $options = isset($config['options']) ? $config['options'] : [];
-
-            return new Filesystem(new OssAdapter($client, $config['bucket'], $url, $prefix, $options));
+        $this->app->resolving('filesystem', function ($filesystem) {
+            $filesystem->extend('aliyun-oss', function ($app, $config) {
+                $client = @new OssClient(
+                    $config['key'],
+                    $config['secret'],
+                    ($url = $config['endpoint']),
+                    $config['cname'],
+                    $config['token']
+                );
+    
+                if (isset($config['url'])) {
+                    $url = $config['url'];
+                } elseif ($config['cname'] === false) {
+                    $url = '';
+                }
+    
+                $prefix = isset($config['prefix']) ? $config['prefix'] : '';
+                $options = isset($config['options']) ? $config['options'] : [];
+    
+                return new Filesystem(new OssAdapter($client, $config['bucket'], $url, $prefix, $options));
+            });
         });
     }
 }
